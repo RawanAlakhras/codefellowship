27ac6968df56ad7ffb52fa1950c74a0e9d173bad
RawanAlakhras <ralakhras36@gmail.com>
2021-08-17 16:29:12 +0300

    lab 16 and 17 done

--- codefellowship/src/main/java/com/example/codefellowship/ApplicationUserController.java
+++ codefellowship/src/main/java/com/example/codefellowship/ApplicationUserController.java
@@ -8,6 +8,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.view.RedirectView;
@@ -26,8 +27,6 @@ public class ApplicationUserController {
 
     @GetMapping("/")
     public String getHome(){
-
-
             return "index.html";
     }
     @GetMapping("/login")
@@ -51,14 +50,22 @@ public class ApplicationUserController {
         ApplicationUser newUser=new ApplicationUser(username,bCryptPasswordEncoder.encode(password),
                 firstName,lastName,dateOfBirth,bio);
         applicationUserRepository.save(newUser);
+
+        //Ensure that user registration also logs users into your app automatically.
         Authentication authentication = new UsernamePasswordAuthenticationToken(newUser, null, new ArrayList<>());
         SecurityContextHolder.getContext().setAuthentication(authentication);
-        return new RedirectView("/login");
+        return new RedirectView("/");
     }
 
     @GetMapping("/profile")
     public String getProfile(Principal p, Model m){
         m.addAttribute("user",applicationUserRepository.findByUsername(p.getName()));
         return "profile.html";
+    }
+
+    @GetMapping("/users/{id}")
+    public String getUserById(@PathVariable ("id") Long id, Model m){
+        m.addAttribute("user", applicationUserRepository.findById(id).get());
+        return "userSearch.html";
     }
 }
--- codefellowship/src/main/java/com/example/codefellowship/CodefellowshipApplication.java
+++ codefellowship/src/main/java/com/example/codefellowship/CodefellowshipApplication.java
@@ -1,11 +1,14 @@
 package com.example.codefellowship;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.web.servlet.error.ErrorMvcAutoConfiguration;
+
+
 
 @SpringBootApplication
 public class CodefellowshipApplication {
-
 	public static void main(String[] args) {
 
 		SpringApplication.run(CodefellowshipApplication.class, args);
